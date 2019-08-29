e22cfb18347108b7459c7dee248fd36c8b6c4e6a
Video <Video@tolmulti12.local>
2019-08-29 08:43:26 -0500

    Initial Commit

---
+++ ToDoList_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList_app
+//
+//  Created by Video on 8/29/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
