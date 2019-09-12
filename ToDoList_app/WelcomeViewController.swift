570d0ac15f8b4a43bd45ac2bbdeb40da82dcbfd2
Video <Video@TOLMULTI02.local>
2019-09-12 09:55:04 -0500

    Avances 5 clase rest api

--- ToDoList_app/WelcomeViewController.swift
+++ ToDoList_app/WelcomeViewController.swift
@@ -8,10 +8,12 @@
 import Foundation
 import UIKit
 
-class WelcomeViewController: UIViewController {
+class WelcomeViewController:UIViewController, NSURLConnectionDelegate, NSURLConnectionDataDelegate{
     
     @IBOutlet weak var lblUser: UILabel!
     @IBOutlet weak var lblClass: UILabel!
+    
+    var data: NSMutableData = NSMutableData();
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +32,7 @@ class WelcomeViewController: UIViewController {
         lblUser.text = dummyVar
         lblClass.text = nameOfClass
         //print("Hola: \(dummyVar)")//n string, c string
+        startConn()
         
     }
     
@@ -42,6 +45,39 @@ class WelcomeViewController: UIViewController {
         
           destination.message = "dsfsdfdsfsdfdsf"
         
+    }
+    /**************/
+    func startConn() {
+        let urlPath:String = "https://raw.githubusercontent.com/joekreatera/apptogo/master/todolist.json"
+        var url: NSURL = NSURL(string: urlPath)!
+        var request: NSURLRequest = NSURLRequest(url: url as URL)
+        var connection: NSURLConnection = NSURLConnection(request: request as URLRequest, delegate: self, startImmediately: false)!
+        print("starting")
+        connection.start()
+    }
+    
+    func connection(_ connection: NSURLConnection, didFailWithError error: Error) {
+        print(error)
+    }
+    
+    func connection(_ connection: NSURLConnection, didReceive data: Data) {
+        print("Trying \(data)")
+        self.data.append(data)
+    }
+    
+    func connectionDidFinishLoading(_ connection: NSURLConnection) {
+        do {
+            let json:NSDictionary = try JSONSerialization.jsonObject(with: self.data as Data, options: .mutableContainers) as! NSDictionary
+            
+            print(json)
+            var dt:NSArray = json["todoListData"] as! NSArray
+            print(dt[0])
+            print(dt[1])
+            print(dt[2])
+            
+        }catch{
+            print("no can do")
+        }
     }
     
 }
